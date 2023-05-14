07ec1c87393de107986a8280bc9e52d6d31fd8e4
DmitriyVoronov1506 <dmitriy.voronov1506@gmail.com>
2023-05-14 20:30:03 +0300

    HW Run

--- World-011/Assets/Scripts/CharacterScript.cs
+++ World-011/Assets/Scripts/CharacterScript.cs
@@ -1,26 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class CharacterScript : MonoBehaviour
 {
     private CharacterController _characterController;
+    private Animator _animator;
     private Vector3 _moveVector;
-    private float _moveSpeed = 150f;
+    private float _moveSpeed = 300f;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
  
     void Update()
     {
-        float factor = _moveSpeed + Time.deltaTime;
-        float ix = Input.GetAxis("Horizontal") * factor;
-        float iy = Input.GetAxis("Vertical") * factor;
+        float factor = _moveSpeed * Time.deltaTime;
+        float ix = Input.GetAxis("Horizontal");
+        float iy = Input.GetAxis("Vertical");
 
-        _moveVector = new Vector3(ix, 0, iy);
+        //_moveVector = new Vector3(ix, 0, iy);         // В мировом пространстве
+
+        _moveVector =
+            this.transform.forward * iy   // составляющая "вперед" - вертикальное управление
+            + this.transform.right * ix;  // вправо - горизонтальное управление
+
+        if (_moveVector.magnitude > 1)
+        {
+            _moveVector = _moveVector.normalized;
+        }
+
+        _moveVector *= factor;
+
+        if (_moveVector.magnitude > _characterController.minMoveDistance)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                _animator.SetInteger("MoveState", 2);
+                _moveVector *= (factor * 4);              
+            }
+            else
+            {
+                _animator.SetInteger("MoveState", 1);
+            }        
+        }
+        else
+        {
+            _animator.SetInteger("MoveState", 0);
+        }
+
         _characterController.SimpleMove(_moveVector);
     }
 }
