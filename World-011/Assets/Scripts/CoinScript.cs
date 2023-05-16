75eae002eb5fecee8da5e4869a670c9a6ba3abe6
DmitriyVoronov1506 <dmitriy.voronov1506@gmail.com>
2023-05-16 19:47:03 +0300

    HW Coin

--- World-011/Assets/Scripts/CoinScript.cs
+++ World-011/Assets/Scripts/CoinScript.cs
@@ -6,6 +6,20 @@ public class CoinScript : MonoBehaviour
 {
     private Animator _animator;
 
+    const float minDistance = 10f;
+    const float maxDistance = 21f;
+
+    const int minDirection = 0;
+    const int maxDirection = 4;
+
+    enum Direction
+    {
+        Forward,
+        Back,
+        Left,
+        Right
+    }
+
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -21,9 +35,77 @@ public class CoinScript : MonoBehaviour
 
     public void Disapeared()
     {
-        //Debug.Log("Disapeared");
 
-        this.transform.position += Vector3.forward * 10;
+        SpawnCoin();
         _animator.SetBool("isCollected", false);
+    }
+
+    private void SpawnCoin()
+    {
+        System.Random random = new System.Random();
+
+        while (true)
+        {
+            random.Next();
+            Direction direction = (Direction)random.Next(minDirection, maxDirection);
+
+            float val = ((float)(random.NextDouble() * (maxDistance - minDistance) + minDistance));
+
+            switch (direction)
+            {
+                case Direction.Forward:
+                    {
+                        Vector3 temp = this.transform.position + Vector3.forward * val;
+
+                        if(temp.z < 965)
+                        {
+                            this.transform.position += Vector3.forward * val;
+                            return;
+                        }
+
+                        break;
+                    }
+
+                case Direction.Back:
+                    {
+                        Vector3 temp = this.transform.position + Vector3.back * val;
+
+                        if (temp.z > 30)
+                        {
+                            this.transform.position += Vector3.back * val;
+                            return;
+                        }
+
+                        break;
+                    }
+
+                case Direction.Left:
+                    {
+                        Vector3 temp = this.transform.position + Vector3.left * val;
+
+                        if (temp.x > 30)
+                        {
+                            this.transform.position += Vector3.left * val;
+                            return;
+                        }
+
+                        break;
+                    }
+
+                case Direction.Right:
+                    {
+                        Vector3 temp = this.transform.position + Vector3.right * val;
+
+                        if (temp.x < 960)
+                        {
+                            this.transform.position += Vector3.right * val;
+                            return;
+                        }
+
+                        break;
+                    }
+
+            }
+        }
     }
 }
